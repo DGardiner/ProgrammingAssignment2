9ffc854ba747525250f873b8f7e7ecff3c20dbdc
DGardiner <dgardiner0821@rogers.com>
2014-05-25 16:01:01 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,17 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## 
+## makeCacheMatrix takes a matrix as a parameter and 
+## returns a list that contains four functions:
+##    set    : sets the matrix to the provided parameter
+##    get    : returns the matrix
+##    setinv : sets the inverse of the matrix
+##    getinv : returns the inverse of the matrix
+## 
+## Assumptions: the provided parameter is a square,
+##              invertible matrix
+##
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
         set <- function(y) {
@@ -19,7 +29,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## 
+## cacheSolve takes a list created by makeCacheMatrix
+## as a parameter, determines the inverse of the matrix,
+## calls setinv from the list and returns the inverse
+##
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         i <- x$getinv()
